a26869ba4f8b0a5ac5de7f4b3cb571bfc5a16d34
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-25 15:20:54 -0400

    Create learn, teach methods  - create person class w/initialize

--- people.rb
+++ people.rb
@@ -1,8 +1,30 @@
-class Student
 
+class Person
+
+  def initialize(name)
+    @name = name
+  end
 
 end
 
-class Instructor
+
+class Student < Person
+
+  def learn
+    puts "I get it!"
+  end
+
+end
+
+class Instructor < Person
+
+  def teach
+
+    puts "Everything in Ruby is an Object!"
+
+  end
+
+
+
 
 end
